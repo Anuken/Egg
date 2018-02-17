45ae94d2466b272ee5d5a3efffcf574e4f896b48
Anuken <arnukren@gmail.com>
2018-02-17 18:40:53 -0500

    less egg

--- src/io/anuke/egg/EggBot.java
+++ src/io/anuke/egg/EggBot.java
@@ -14,7 +14,7 @@ public class EggBot{
 	String token;
 	IDiscordClient client;
 	
-	double eggChance = 1.0 / 400.0;
+	double eggChance = 1.0 / 600.0;
 	
 	public EggBot(){
 		token = System.getProperty("token");
