4874984647e2df8c02b2deef13ec12d17455e827
Anuken <arnukren@gmail.com>
2020-09-23 17:36:47 -0400

    egg

--- egg.js
+++ egg.js
@@ -6,28 +6,21 @@ const client = new Discord.Client();
 let eggCount = 0;
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({
-    game: { 
-	  name: `${eggCount} eggs.`,
-	  type: 'WATCHING'
-	},
-	status: 'idle'
-  })
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if(msg.content.includes("egg")){
-	msg.react('🥚');
-	eggCount++;
-	client.user.setPresence({
-      game: { 
-        name: `${eggCount} 🥚`,
-        type: 'WATCHING'
-      },
-      status: 'idle'
-    })
-  }
+	if(msg.content.includes("egg")){
+		msg.react('🥚');
+		eggCount++;
+		client.user.setPresence({
+			game: { 
+				name: `${eggCount} 🥚`,
+				type: 'WATCHING'
+			},
+			status: 'idle'
+		})
+	}
 });
 
 client.login(process.env.EGG_BOT_TOKEN);
