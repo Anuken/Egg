373a7bbe8cb1a51b064c315a5a5dc09cd9f2a95a
LeoDog896 <LeoDog896@hotmail.com>
2020-09-23 09:21:05 -0400

    Egg counter
    This adds an egg counter to the presence. Also,
    No, the package-lock.json SHOULD NOT be added to .gitignore.
    Also ignore .env

--- egg.js
+++ egg.js
@@ -3,13 +3,30 @@ require("dotenv"); // Load env configuration
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let eggCount = 0;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    game: { 
+	  name: `${eggCount} eggs.`,
+	  type: 'WATCHING'
+	},
+	status: 'idle'
+  })
 });
 
 client.on('message', msg => {
   if(msg.content.includes("egg")){
-    msg.react('🥚');
+	msg.react('🥚');
+	eggCount++;
+	client.user.setPresence({
+      game: { 
+        name: `${eggCount} eggs.`,
+        type: 'WATCHING'
+      },
+      status: 'idle'
+    })
   }
 });
 
