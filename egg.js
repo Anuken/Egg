94fa622a163acfbfb1dac9886c7decda2dcec9df
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-09-26 22:38:51 +0800

    added new feature to remove eggs, made updating presence a different function

--- egg.js
+++ egg.js
@@ -1,4 +1,4 @@
-require("dotenv"); // Load env configuration
+require('dotenv'); // Load env configuration
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -6,20 +6,29 @@ const client = new Discord.Client();
 let eggCount = 0;
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-	if(msg.content.toLowerCase().includes("egg")){
-		msg.react('🥚');
-		eggCount++;
-		client.user.setPresence({
-			activity: { 
-				name: `${eggCount} 🥚`
-			},
-			status: 'online'
-		})
-	}
+client.on('message', (msg) => {
+    let content = msg.content.toLowerCase();
+    if (content.includes('egg')) {
+        msg.react('🥚');
+        eggCount++;
+        updatePresence(eggCount);
+    } else if (content.includes('omlette')) {
+        msg.react('🍴');
+        eggCount--;
+        updatePresence(eggCount);
+    }
 });
 
 client.login(process.env.EGG_BOT_TOKEN);
+
+function updatePresence(eggCount) {
+    client.user.setPresence({
+        activity: {
+            name: `${eggCount} 🥚`,
+        },
+        status: 'online',
+    });
+}
